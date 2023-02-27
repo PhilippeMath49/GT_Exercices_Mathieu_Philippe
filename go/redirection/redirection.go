2b80c4bebfb6a2d672171910fbdb4a39b4b1ec13
PhilippeMath49 <philippe.mathieu@ynov.com>
2023-02-27 17:20:39 +0100

    carousel js

--- go/redirection/redirection.go
+++ go/redirection/redirection.go
@@ -5,6 +5,12 @@ import (
 	"text/template"
 )
 
+type Data struct {
+	SwitchPage2 func()
+	SwitchPage3 func()
+	backToMain  func()
+}
+
 func main() {
 	tmpl1 := template.Must(template.ParseFiles("page.html"))
 	tmpl2 := template.Must(template.ParseFiles("page2.html"))
@@ -26,7 +32,19 @@ func main() {
 			tmpl3.Execute(w, nil)
 			return
 		}
-		http.Redirect()
+
 	})
 	http.ListenAndServe(":80", nil)
+}
+
+func SwitchPage2(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/page2", http.StatusSeeOther)
+}
+
+func SwitchPage3(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/page3", http.StatusSeeOther)
+}
+
+func backToMain(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
