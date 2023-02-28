f10b69c626ae78dceb241b89bea631a303a59ea7
PhilippeMath49 <philippe.mathieu@ynov.com>
2023-02-28 12:24:10 +0100

    redirection

--- go/redirection/redirection.go
+++ go/redirection/redirection.go
@@ -1,50 +1,53 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"text/template"
 )
-
-type Data struct {
-	SwitchPage2 func()
-	SwitchPage3 func()
-	backToMain  func()
-}
 
 func main() {
 	tmpl1 := template.Must(template.ParseFiles("page.html"))
 	tmpl2 := template.Must(template.ParseFiles("page2.html"))
 	tmpl3 := template.Must(template.ParseFiles("page3.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			errorHandler(w, r, http.StatusNotFound)
+			return
+
+		}
 		if r.Method != http.MethodPost {
 			tmpl1.Execute(w, nil)
 			return
 		}
 	})
 	http.HandleFunc("/page2", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/page2" {
+			tmpl2.Execute(w, nil)
+			return
+		}
 		if r.Method != http.MethodPost {
 			tmpl2.Execute(w, nil)
 			return
 		}
 	})
 	http.HandleFunc("/page3", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/page3" {
+			tmpl2.Execute(w, nil)
+			return
+		}
 		if r.Method != http.MethodPost {
 			tmpl3.Execute(w, nil)
 			return
 		}
-
 	})
+
 	http.ListenAndServe(":80", nil)
 }
 
-func SwitchPage2(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/page2", http.StatusSeeOther)
-}
-
-func SwitchPage3(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/page3", http.StatusSeeOther)
-}
-
-func backToMain(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/", http.StatusSeeOther)
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	w.WriteHeader(status)
+	if status == http.StatusNotFound {
+		fmt.Fprint(w, "ERROR 404")
+	}
 }
